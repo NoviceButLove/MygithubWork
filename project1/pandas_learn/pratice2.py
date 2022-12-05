c6b2fd6c68526b3f1b4a8ff79db2da3d5e5a469c
NoviceButLove <2912154279@qq.com>
2022-12-05 23:32:19 +0800

    new learn new knowledge

---
+++ project1/pandas_learn/pratice2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+from pandas import *
+from numpy import *
+
+data = DataFrame(np.arange(16).reshape(4,4),
+    index=list('ABCD'),columns = list('abcd'))
+print(data)
+print('---===========-----------')
+print(data.loc['A'])#注意区别，此处一个一个输出
+print('--------------------------')
+print(data.loc[['A']])#此处输出一行
+print('-------------------------------')
+print(data.loc[['B'],['b']])#这种格式，输出行列确定的一个值
+print('==------------=================------')
+print(data.loc[:,'a'])#这种格式，输出a列所有行
