efbe0fd4846bab1172589b49a829a6131dce5dfc
NoviceButLove <2912154279@qq.com>
2022-12-05 09:45:21 +0800

    study my work

---
+++ project1/pandas_learn/pandas_pratice.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import openpyxl
+file1 = pd.read_excel(r'C:\Users\86151\Desktop\File_Test\pandas_test.xlsx')
+print(file1)
---
+++ project1/pandas_learn/read_csv_file.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv(r'D:\LJCWork\BeginWithTechnique\Biginvent_work\ratings_CDs_and_Vinyl.csv')
+#print(df.head())#head 查看前几行的数据
+print(df.columns)
