33cf29f22163fc878a15c059a30b604dd8892a86
NoviceButLove <2912154279@qq.com>
2022-12-05 11:43:08 +0800

    new learn new knowledge

--- project1/pandas_learn/pandas_pratice.py
+++ project1/pandas_learn/pandas_pratice.py
@@ -26,19 +26,33 @@ file1.loc[:,'分数'] = 100
 print(file1.loc[file1['疫情风险等级'] == '常态化防控地区', '姓名':'分数'])
 
 #print(file1.head())
+print('--------------------')
+'''
+下面是loc的另一种创建列，加上series的方式
+loc[：->选择所有行 ，’新列的名称‘]apply（函数名，坐标轴）
+'''
 def plus_score(df):
     if df['疫情风险等级'] == '常态化防控地区':
         return 'good'
     else:
         return 'so-so'
 file1.loc[:, 'plusscore'] = file1.apply(plus_score, axis=1)
+print(file1.head())
 #print(file1)
+print('-----------------------------')
+print(file1['plusscore'].value_counts())#查看plusscore 枚举结果的个数
+print(file1['plusscore'].value_counts('so-so'))#查看plusscore 指定结果的百分比
 
+print('-------------------------')
 
-
-
-
-
+#print(file1.describe())
+print(file1['plusscore'].unique())#查看独特值
+print(file1['plusscore'].value_counts())#各个值计数
+'''
+file1.cov()协方差矩阵
+file1.corr()相关系数矩阵 
+！！！！数据挖掘后（计算特定的方差或系数）可用作于机器学习
+'''
 
 
 
