709dfda7488b3655dda13f250c300968ddccab2d
NoviceButLove <2912154279@qq.com>
2022-12-05 10:39:31 +0800

    nice try

--- project1/pandas_learn/pandas_pratice.py
+++ project1/pandas_learn/pandas_pratice.py
@@ -1,4 +1,17 @@
 import pandas as pd
 import openpyxl
 file1 = pd.read_excel(r'C:\Users\86151\Desktop\File_Test\pandas_test.xlsx')
-print(file1)
+#print(file1.head())
+print('--------------------')
+
+print(file1.loc[:, '日期'])
+print('----------------')
+print(file1.index)
+print(file1.columns)
+#file1.loc[:, '日期'] = file1['日期'].str.replace('-', '')这里数据有问题，所以报错，datatime的数据
+#print(file1)
+#file1.set_index()可以设置列索引
+print(file1.loc[23,'姓名'])#用行坐标加列坐标确定
+print('----------------------------')
+print(file1.loc[file1['疫情风险等级'] == '常态化防控地区', :])
+
