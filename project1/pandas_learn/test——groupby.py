829b017b93191c56258baf7e2d44897787ce6889
NoviceButLove <2912154279@qq.com>
2022-12-07 00:25:09 +0800

    new learn new knowledge

---
+++ project1/pandas_learn/test——groupby.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+df = pd.DataFrame([[4,9]] * 3, columns=['A', 'B'])
+
+print(df)
+print('-------------------------')
+'''
+lambda定义无名函数，np.sum()返回一个series
+同时，df.apply 是创建了一个新的对象
+所以打印df还是之前那个
+'''
+df1 = df.apply(lambda x: np.sum(x), axis=1)
+print(df1)
+print(type(df1))
+
+print('---------------------')
+def cubic(x):
+    return x**3
+df2 = df.apply(cubic)
+print(df2)
+
+print('-----------------------------')
+file1 = r'C:\Users\86151\Desktop\File_Test\enligh_test.xlsx'
+gp = pd.read_excel(file1)
+gp0 = gp.dropna(axis=0, how='any')
+gp1 = gp0.groupby('授课教师')
+#print(type(gp['我的练习时长']))
+gp1 = gp1['我的练习完成度'].mean()
+#gp1 = gp1.sort_values(ascending=False)
+#print(gp1)
+
+
